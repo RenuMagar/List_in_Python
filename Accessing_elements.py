41c2f91c02fff8b45477802e543f9c5f6b6ed50c
RenuMagar <145140786+RenuMagar@users.noreply.github.com>
2023-10-02 18:51:58 +0530

    Create Accessing_elements.py

---
+++ Accessing_elements.py
@@ -0,0 +1,8 @@
+list8=['flowers', 'are', 'red',"yellow",1.34,67]
+print(list8[1]) #using index
+print(list8[-2])
+print(list8[0:4]) 
+print(list8[-1:]) 
+print(list8[:4])
+print(list8[:-1])
+print(list8[-3:])
